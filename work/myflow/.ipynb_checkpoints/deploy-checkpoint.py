345418c7d94ef5a925299c10eff1bb3bf961fb2f
Your Name <you@example.com>
2025-05-11 16:24:15 +0700

    update flow

--- work/myflow/.ipynb_checkpoints/deploy-checkpoint.py
+++ work/myflow/.ipynb_checkpoints/deploy-checkpoint.py
@@ -1,20 +1,10 @@
-# from prefect import flow
-# from pathlib import Path
-# from flow import main_flow
-
-# if __name__ == "__main__":
-#     main_flow.deploy(
-#         name="dust_deployment",
-#         parameters={},
-#         work_pool_name="default-agent-pool",
-#         schedule={"cron": "0 * * * *"},  # Run every hour
-#     )
-
 from prefect import flow
 from pathlib import Path
 
 source = str(Path.cwd())
-entrypoint = f"flow.py:main_flow"  # python file: function
+
+entrypoint = f"flow.py:main_flow"
+
 print(f'entrypoint:{entrypoint}, source:{source}')
 
 if __name__ == "__main__":
@@ -22,8 +12,8 @@ if __name__ == "__main__":
         source=source,
         entrypoint=entrypoint,
     ).deploy(
-        name="dust_deployment",
+        name="pollution_deployment",
         parameters={},
         work_pool_name="default-agent-pool",
-        cron="0 * * * *",  # Run every 5 minutes
+        cron="3 * * * *", 
     )
